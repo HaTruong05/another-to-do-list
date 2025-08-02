9c706274eefacb0b128251c90398987b3e9ca9be
Ha Truong <125408638+HaTruong05@users.noreply.github.com>
2025-08-02 15:33:16 -0700

    Improve styling.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import "./styles.css";
 lucide.createIcons();
 
 
-const addTaskButton = document.getElementById('new-task');
-const content = document.getElementById('content');
-addTaskButton.addEventListener('click', () => {
-    content.innerHTML = '';
-    const taskTitle = document.createElement('h1');
+// const addTaskButton = document.getElementById('new-task');
+// const content = document.getElementById('content');
+// addTaskButton.addEventListener('click', () => {
+//     content.innerHTML = '';
+//     const taskTitle = document.createElement('h1');
     
 
-})
+// })
+
+// function 
